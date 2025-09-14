79e3763f6c883f3d0426481541cf9c0b563d1388
balfiere <balfiere@proton.me>
2025-09-14 16:41:39 -0500

    update config

--- quartz.config.ts
+++ quartz.config.ts
@@ -83,7 +83,7 @@ const config: QuartzConfig = {
       Plugin.FolderPage(),
       Plugin.TagPage(),
       Plugin.ContentIndex({
-        enableSiteMap: true,
+        enableSiteMap: true,y
         enableRSS: false,
       }),
       Plugin.Assets(),
